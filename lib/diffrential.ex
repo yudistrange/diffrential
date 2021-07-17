dc73f1d25e0cb845caad5e86de99c4bb794b9f9c
udit <yudistrange@gmail.com>
2021-07-17 22:16:57 +0530

    Initial commit

---
+++ lib/diffrential.ex
@@ -0,0 +1,18 @@
+defmodule Diffrential do
+  @moduledoc """
+  Documentation for `Diffrential`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Diffrential.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/diffrential_test.exs
@@ -0,0 +1,8 @@
+defmodule DiffrentialTest do
+  use ExUnit.Case
+  doctest Diffrential
+
+  test "greets the world" do
+    assert Diffrential.hello() == :world
+  end
+end
